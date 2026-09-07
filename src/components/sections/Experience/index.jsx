5609c1656b48918f334b1b276e2c446f00d351fa
AndrewCrescencio <andrew_workstation@hotmail.com>
2026-09-07 18:41:46 -0300

    fix: remove duplicate Projects rendering from Experience section

--- src/components/sections/Experience/index.jsx
+++ src/components/sections/Experience/index.jsx
@@ -1,12 +1,9 @@
 import { Companies } from "@/components/UI/Companies";
-import { Company } from "@/components/UI/Company";
-import { Projects } from "../Projects";
 
-export const Experience = ({ companies, projects }) => {
+export const Experience = ({ companies }) => {
   return (
     <>
       {companies && <Companies data={companies} />}
-      {projects?.length > 0 && <Projects data={projects} />}
     </>
   );
 };
